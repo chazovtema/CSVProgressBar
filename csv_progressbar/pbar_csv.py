b180f104e1fc4c07665cddb0d2dff5fe1846d13b
chazovtema <chazovtema@mail.ru>
2024-01-16 21:17:54 +0500

    Ускорил быстродействие на 30 процентов (сейчас функция чтения на ~30 процентов медленее)

--- csv_progressbar/pbar_csv.py
+++ csv_progressbar/pbar_csv.py
@@ -10,6 +10,50 @@ from .pbar_protocol import PbarProtocol
 _DialectLike: TypeAlias = str | Dialect | type[Dialect]
 _QuotingType: TypeAlias = int
 
+def iter_csv(csvfile: TextIOWrapper,
+        pbar: PbarProtocol,
+        update_rate: int = 32768,
+        dialect: _DialectLike = "excel",
+        *,
+        delimiter: str = ",",
+        quotechar: str | None = '"',
+        escapechar: str | None = None,
+        doublequote: bool = True,
+        skipinitialspace: bool = False,
+        lineterminator: str = "\r\n",
+        quoting: _QuotingType = 0,
+        strict: bool = False):
+    
+    fd = csvfile.fileno()
+    readed_rows = 0
+    last_position = 0
+    reader = csv.reader(csvfile, 
+                                 dialect, 
+                                 delimiter= delimiter,
+                                 quotechar= quotechar,
+                                 escapechar= escapechar,
+                                 doublequote= doublequote,
+                                 skipinitialspace= skipinitialspace,
+                                 lineterminator = lineterminator,
+                                 quoting= quoting,
+                                 strict= strict)
+
+    for row in reader:
+        readed_rows += 1
+        if readed_rows == update_rate:
+            pos = os.lseek(fd, 0, os.SEEK_CUR)
+            last_pos = last_position
+            if last_pos != pos:
+                pbar.update(pos - last_pos)
+                last_position = pos
+                readed_rows = 0
+        yield row
+        
+    if readed_rows != 0:
+        pos = os.lseek(fd, 0, os.SEEK_CUR)
+        last_pos = last_position
+        if last_pos != pos:
+            pbar.update(pos - last_pos)
 
 class PbarCSV:
     
@@ -27,41 +71,33 @@ class PbarCSV:
         quoting: _QuotingType = 0,
         strict: bool = False) -> None:
 
-        self.fd = csvfile.fileno()
-        self.pbar = pbar
-        self.update_rate = update_rate
-        self.rows = 0
-        self.last_position = 0
-        self.reader = csv.reader(csvfile, 
-                                 dialect, 
-                                 delimiter= delimiter,
-                                 quotechar= quotechar,
-                                 escapechar= escapechar,
-                                 doublequote= doublequote,
-                                 skipinitialspace= skipinitialspace,
-                                 lineterminator = lineterminator,
-                                 quoting= quoting,
-                                 strict= strict)
+        self.reader = iter_csv(csvfile,
+                                pbar,
+                                update_rate,
+                                dialect, 
+                                delimiter= delimiter,
+                                quotechar= quotechar,
+                                escapechar= escapechar,
+                                doublequote= doublequote,
+                                skipinitialspace= skipinitialspace,
+                                lineterminator = lineterminator,
+                                quoting= quoting,
+                                strict= strict)
+        # self.reader = csv.reader(csvfile, 
+        #                          dialect, 
+        #                          delimiter= delimiter,
+        #                          quotechar= quotechar,
+        #                          escapechar= escapechar,
+        #                          doublequote= doublequote,
+        #                          skipinitialspace= skipinitialspace,
+        #                          lineterminator = lineterminator,
+        #                          quoting= quoting,
+        #                          strict= strict)
         
     def __next__(self):
-        self.rows += 1
-        if self.rows == self.update_rate:
-            pos = os.lseek(self.fd, 0, os.SEEK_CUR)
-            last_pos = self.last_position
-            if last_pos != pos:
-                self.pbar.update(pos - last_pos)
-                self.last_position = pos
-                self.rows = 0
-        try:
-            return next(self.reader)
-        except StopIteration:
-            pos = os.lseek(self.fd, 0, os.SEEK_CUR)
-            last_pos = self.last_position
-            if last_pos != pos:
-                self.pbar.update(pos - last_pos)
-            raise
+        return next(self.reader)
         
     def __iter__(self):
-        return self
+        return self.reader
         
         
